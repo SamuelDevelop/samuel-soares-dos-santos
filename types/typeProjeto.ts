115badad0c9acf65ed4e056382f3c9adbb34d43f
SamuelDevelop <ssoaresdossantos088@gmail.com>
2026-04-07 18:38:09 -0300

    adicionando daods e svg's de tecnologias

---
+++ types/typeProjeto.ts
@@ -0,0 +1,11 @@
+export type Projeto = {
+    nome : string,
+    descrição: string,
+    tecnologia : string,
+    link_repo  : string,
+    temTeste : boolean,
+    link_teste : string,
+    aprox_data : string,
+    temImagem: boolean,
+    imagem_link : string,
+}
