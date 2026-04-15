2a462c5dc15259918395d7544f292d075759cca0
SamuelDevelop <ssoaresdossantos088@gmail.com>
2026-04-14 23:22:44 -0300

    atualizando as descrições de skills

--- app/skills/page.tsx
+++ app/skills/page.tsx
@@ -22,9 +22,7 @@ function Skills(){
                 <>
                     <h1>Pratileira 1:</h1>
                     
-                    <p>Falamos também sobre um conjunto de vivências,</p>
-                    <p>Que contribuem para que estas habilidades sejam desenvolvidas, assim sendo resolvi organiza-las em pratileiras.</p>
-                    <p>Cada pratileira conta um pouquinha da históra de como achegamos até ali.</p>
+                    <p>São as tecnologias que mais utilizei durante meu processo de aprendizado, que julgo possuir um maior conhecimento comparadas as demais e muito dificilmente deixarei de usar.</p>
                 </>
                 } 
                 colorClass="bgDark"
@@ -35,9 +33,7 @@ function Skills(){
                 <>
                     <h1>Pratileira 2:</h1>
                     
-                    <p>Falamos também sobre um conjunto de vivências,</p>
-                    <p>Que contribuem para que estas habilidades sejam desenvolvidas, assim sendo resolvi organiza-las em pratileiras.</p>
-                    <p>Cada pratileira conta um pouquinha da históra de como achegamos até ali.</p>
+                    <p>São as tecnologias que mais estou utilizando recentemente, que mais tenho dedicado esforços, que pretendo trabalhar fututamente mas que ainda estou em processo de aprendizagem.</p>
                 </>
                 } 
                 colorClass="bgPurple"
@@ -48,9 +44,7 @@ function Skills(){
                 <>
                     <h1>Pratileira 3:</h1>
                     
-                    <p>Falamos também sobre um conjunto de vivências,</p>
-                    <p>Que contribuem para que estas habilidades sejam desenvolvidas, assim sendo resolvi organiza-las em pratileiras.</p>
-                    <p>Cada pratileira conta um pouquinha da históra de como achegamos até ali.</p>
+                    <p>São tecnologias que utilizo ou já utilizei em alguns contextos, seja para aprendizado, seja para algum projeto, mas sem nenhuma grande pretenção futura aparente.</p>
                 </>
                 } 
                 colorClass="bgDark"
@@ -61,9 +55,7 @@ function Skills(){
                 <>
                     <h1>Pratileira 4:</h1>
                     
-                    <p>Falamos também sobre um conjunto de vivências,</p>
-                    <p>Que contribuem para que estas habilidades sejam desenvolvidas, assim sendo resolvi organiza-las em pratileiras.</p>
-                    <p>Cada pratileira conta um pouquinha da históra de como achegamos até ali.</p>
+                    <p>São tecnologias que eu aprendi, já tive que estudar e até fazer projetos, mas que já faz algum tempo que não utilizei mais.</p>
                 </>
                 } 
                 colorClass="bgPurple"
